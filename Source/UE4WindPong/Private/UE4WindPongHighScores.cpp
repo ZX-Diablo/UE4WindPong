a879b9b7eaddf7989a5a95f3e082fb1d58517cfc
ZX_Diablo <yakut2004@bk.ru>
2018-02-07 23:15:08 +0500

    High scores implementation (USaveGame)

--- Source/UE4WindPong/Private/UE4WindPongHighScores.cpp
+++ Source/UE4WindPong/Private/UE4WindPongHighScores.cpp
@@ -1,3 +1,24 @@
 // Copyright ZX.Diablo 2018
 
 #include "Public/UE4WindPongHighScores.h"
+
+const FString UUE4WindPongHighScores::SlotName = TEXT("highscores");
+const uint32 UUE4WindPongHighScores::MaxEntries = 10;
+
+UUE4WindPongHighScores::UUE4WindPongHighScores ()
+{
+	this->HighScoreEntries.Init({}, UUE4WindPongHighScores::MaxEntries);
+}
+
+void UUE4WindPongHighScores::SetEntry (uint32 Position, const FUE4WindPongHighScoreEntry& Entry)
+{
+	if (Position < UUE4WindPongHighScores::MaxEntries)
+	{
+		this->HighScoreEntries[Position] = Entry;
+	}
+}
+
+const TArray<FUE4WindPongHighScoreEntry>& UUE4WindPongHighScores::GetEntries () const
+{
+	return this->HighScoreEntries;
+}
--- Source/UE4WindPong/Public/UE4WindPongHighScores.h
+++ Source/UE4WindPong/Public/UE4WindPongHighScores.h
@@ -6,6 +6,21 @@
 #include "GameFramework/SaveGame.h"
 #include "UE4WindPongHighScores.generated.h"
 
+USTRUCT(BlueprintType)
+struct FUE4WindPongHighScoreEntry
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	/** Player name */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, SaveGame)
+	FString PlayerName;
+
+	/** Player score */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, SaveGame)
+	float Score;
+};
+
 /**
  * 
  */
@@ -14,4 +29,30 @@ class UE4WINDPONG_API UUE4WindPongHighScores : public USaveGame
 {
 	GENERATED_BODY()
 
+public:
+	UUE4WindPongHighScores ();
+
+	/**
+	 * Set value for high scores entry at given position
+	 * @param Position Position in high scores array, should be less than MaxEntries
+	 * @param Entry New entry value
+	 */
+	void SetEntry (uint32 Position, const FUE4WindPongHighScoreEntry& Entry);
+
+	/**
+	 * Get all high scores entries
+	 * @return High scores array
+	 */
+	const TArray<FUE4WindPongHighScoreEntry>& GetEntries () const;
+
+public:
+	/** Save slot name */
+	static const FString SlotName;
+
+	/** Maximum allowed number of high scores entries */
+	static const uint32 MaxEntries;
+
+private:
+	UPROPERTY()
+	TArray<FUE4WindPongHighScoreEntry> HighScoreEntries;
 };
