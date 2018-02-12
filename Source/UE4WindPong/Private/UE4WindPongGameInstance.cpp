63dd24105f62830fa6b56c3436dfb38fd21fa4db
ZX_Diablo <yakut2004@bk.ru>
2018-02-12 23:29:07 +0500

    Game instance methods to work with high scores from blueprints

--- Source/UE4WindPong/Private/UE4WindPongGameInstance.cpp
+++ Source/UE4WindPong/Private/UE4WindPongGameInstance.cpp
@@ -1,3 +1,36 @@
 // Copyright ZX.Diablo 2018
 
 #include "Public/UE4WindPongGameInstance.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void UUE4WindPongGameInstance::SetHighScore (int32 Position, const FUE4WindPongHighScoreEntry& Entry)
+{
+	this->CacheHighScores();
+
+	if (Position >= 0)
+	{
+		this->HighScores->SetEntry(Position, Entry);
+
+		UGameplayStatics::SaveGameToSlot(this->HighScores, UUE4WindPongHighScores::SlotName, 0);
+	}
+}
+
+const TArray<FUE4WindPongHighScoreEntry>& UUE4WindPongGameInstance::GetHighScores () const
+{
+	this->CacheHighScores();
+
+	return this->HighScores->GetEntries();
+}
+
+void UUE4WindPongGameInstance::CacheHighScores () const
+{
+	if (!this->HighScores)
+	{
+		this->HighScores = Cast<UUE4WindPongHighScores>(UGameplayStatics::LoadGameFromSlot(UUE4WindPongHighScores::SlotName, 0));
+		if (!this->HighScores)
+		{
+			this->HighScores = Cast<UUE4WindPongHighScores>(UGameplayStatics::CreateSaveGameObject(UUE4WindPongHighScores::StaticClass()));
+		}
+	}
+}
--- Source/UE4WindPong/Public/UE4WindPongGameInstance.h
+++ Source/UE4WindPong/Public/UE4WindPongGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Public/UE4WindPongHighScores.h"
 #include "UE4WindPongGameInstance.generated.h"
 
 /**
@@ -14,4 +15,26 @@ class UE4WINDPONG_API UUE4WindPongGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
+public:
+	/**
+	 * Set new high score and save
+	 * @param Position High score position
+	 * @param Entry High score value
+	 */
+	UFUNCTION(BlueprintCallable, Category = "High Scores")
+	void SetHighScore (int32 Position, const FUE4WindPongHighScoreEntry& Entry);
+
+	/**
+	 * Load and get array of high scores
+	 * @return High scores
+	 */
+	UFUNCTION(BlueprintCallable, Category = "High Scores")
+	const TArray<FUE4WindPongHighScoreEntry>& GetHighScores () const;
+
+private:
+	void CacheHighScores () const;
+
+private:
+	UPROPERTY()
+	mutable UUE4WindPongHighScores* HighScores;
 };
